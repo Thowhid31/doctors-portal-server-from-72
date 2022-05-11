ead0381fdfa40a0a1bd8f4513d53950ee613f0d2
Thowhid31 <thowhidantor16@gmail.com>
2022-05-11 09:34:07 +0600

    implement services thtough database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const query = require('express/lib/middleware/query');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -9,7 +10,28 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://doctor_admin:<password>@cluster0.nf7el.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nf7el.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        await client.connect();
+        const serviceCollection = client.db('doctors_portal').collection('services');
+
+        app.get('/service', async(req, res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World from Doc Uncle!')
