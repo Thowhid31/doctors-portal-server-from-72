16a7a6730f138758f89b286da2bc77864e39d037
Thowhid31 <thowhidantor16@gmail.com>
2022-05-11 09:08:25 +0600

    connetion with mongodb is primary state

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = "mongodb+srv://doctor_admin:<password>@cluster0.nf7el.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 app.get('/', (req, res) => {
   res.send('Hello World from Doc Uncle!')
