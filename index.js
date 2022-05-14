3b8f263b265cb3451cc1c61d7548dd2790f9389e
Thowhid31 <thowhidantor16@gmail.com>
2022-05-14 22:48:42 +0600

    added put for update one

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ async function run() {
     await client.connect();
     const serviceCollection = client.db('doctors_portal').collection('services');
     const bookingCollection = client.db('doctors_portal').collection('booking');
+    const userCollection = client.db('doctors_portal').collection('users');
 
     app.get('/service', async (req, res) => {
       const query = {};
@@ -26,6 +27,17 @@ async function run() {
       const services = await cursor.toArray();
       res.send(services);
     });
+
+    app.put('/user/:email', async(req, res)=>{
+      const email = req.params.email;
+      const user = req.body;
+      const filter = {email: email}
+      const options = {upsert: true}
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options)
+    })
 
     app.get('/available', async(req, res)=>{
       const date = req.query.date;
