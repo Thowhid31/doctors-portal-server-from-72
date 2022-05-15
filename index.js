db3c715c0459549b80924e9fbc899d0b60b79129
Thowhid31 <thowhidantor16@gmail.com>
2022-05-15 09:38:34 +0600

    implement jwt for myappointment dashboard

--- index.js
+++ index.js
@@ -14,6 +14,20 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nf7el.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'Unauthorized Access' })
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden Access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 async function run() {
   try {
@@ -29,32 +43,32 @@ async function run() {
       res.send(services);
     });
 
-    app.put('/user/:email', async(req, res)=>{
+    app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
       const user = req.body;
-      const filter = {email: email}
-      const options = {upsert: true}
+      const filter = { email: email }
+      const options = { upsert: true }
       const updateDoc = {
         $set: user,
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '24h'})
-      res.send({result, token});
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h' })
+      res.send({ result, token });
     })
 
-    app.get('/available', async(req, res)=>{
+    app.get('/available', async (req, res) => {
       const date = req.query.date;
 
       // step-1 : get all  services
       const services = await serviceCollection.find().toArray();
 
       //step-2 : get the booking of that day
-      const query = {date: date};
+      const query = { date: date };
       const bookings = await bookingCollection.find(query).toArray();
 
       //step-3: for each service
       services.forEach(service => {
-        
+
         //step-4: find booking for that service
         const serviceBookings = bookings.filter(book => book.treatment === service.name);
 
@@ -82,25 +96,29 @@ async function run() {
      * app.delete('/booking/:id) //
      */
 
-    app.get('/booking', async(req, res)=>{
+    app.get('/booking', verifyJWT, async (req, res) => {
       const patient = req.query.patient;
       const authorization = req.headers.authorization;
-      console.log('auth header', authorization);
-      const query = {patient: patient};
-      const bookings = await bookingCollection.find(query).toArray();
-
-      res.send(bookings)
+      const decodedEmail = req.decoded.email;
+      if (patient === decodedEmail) {
+        const query = { patient: patient };
+        const bookings = await bookingCollection.find(query).toArray();
+        return res.send(bookings)
+      }
+      else {
+        return res.status(403).send({ message: 'Forbidden Access' })
+      }
     })
 
-    app.post('/booking', async(req, res)=>{
+    app.post('/booking', async (req, res) => {
       const booking = req.body;
-      const query = {treatment: booking.treatment, date: booking.date, patient: booking.patient};
+      const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient };
       const exists = await bookingCollection.findOne(query);
-      if(exists){
-        return res.send({success: false, booking: exists})
+      if (exists) {
+        return res.send({ success: false, booking: exists })
       }
       const result = await bookingCollection.insertOne(booking);
-      return res.send({success: true, result});
+      return res.send({ success: true, result });
     })
   }
   finally {
